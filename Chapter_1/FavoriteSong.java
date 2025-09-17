075336fa6678a7533b419d4e8809a62aac2de1df
NestlieDelaPena-UCM <nestlierocaldelapena@gmail.com>
2025-09-18 00:21:32 +0800

    Update FavoriteSong.java
    revised lyrics

--- Chapter_1/FavoriteSong.java
+++ Chapter_1/FavoriteSong.java
@@ -1,8 +1,8 @@
 public class FavoriteSong {
     public static void main(String[] args) {
-        System.out.println("Shining bright in the midnight sky,");
-        System.out.println("Dreams take flight as the moments pass by,");
-        System.out.println("Every step leads to something new,");
-        System.out.println("And the world feels alive with you.");
+        System.out.println("Sasayaw sa kulog at ulan, iikutin ang tala at buwan");
+        System.out.println("Habang tayo ay naliligaw, pakinggan ang puso, 'wag nang bibitaw");
+        System.out.println("'Wag nang magtagu-taguan, kita naman sa liwanag ng buwan");
+        System.out.println("Ang lihim na pagtingin (ang lihim), kailan aaminin?");
     }
 }
