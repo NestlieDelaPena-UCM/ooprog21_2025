3310701ead0987e440b91be3e13b4c1fd5d9a398
NestlieDelaPena_UCM <nestlierocaldelapena@gmail.com>
2025-10-21 17:17:48 +0800

    Add Chapter_6 with BankBalance.java

---
+++ Chapter_6/BankBalance.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BankBalance {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double interestRate = 0.03;
+        double balance;
+        int year = 1;
+        int choice;
+
+        System.out.print("Enter initial bank balance > ");
+        balance = input.nextDouble();
+
+        System.out.print("Do you want to see next year's balance? \nEnter 1 for yes or any other number for no >> ");
+        choice = input.nextInt();
+
+        while (choice == 1) {
+            balance = balance + (balance * interestRate);
+            System.out.printf("After year %d at %.2f interest rate, balance is $%.2f%n", year, interestRate, balance);
+
+            year++;
+            System.out.print("\nDo you want to see the balance at the end of another year?\nEnter 1 for yes or any other number for no >> ");
+            choice = input.nextInt();
+        }
+
+        input.close();
+    }
+}
