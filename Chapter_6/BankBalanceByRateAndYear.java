cf06fc59523bc01d07b2a80cebc47e18ef84be45
NestlieDelaPena-UCM <nestlierocaldelapena@gmail.com>
2025-10-29 18:35:38 +0800

    Add files via upload

---
+++ Chapter_6/BankBalanceByRateAndYear.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BankBalanceByRateAndYear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter initial bank balance > ");
+        double balance = input.nextDouble();
+
+        double[] interestRates = {0.02, 0.03, 0.04, 0.05};
+        
+        for (int i = 0; i < interestRates.length; i++) {
+            double rate = interestRates[i];
+            double newBalance = balance;
+
+            System.out.println("\nWith an initial balance of $" + balance + " at an interest rate of " + rate);
+            
+            for (int year = 1; year <= 4; year++) {
+                newBalance = newBalance + (newBalance * rate);
+                System.out.println("After year " + year + " balance is $" + newBalance);
+            }
+        }
+
+        input.close();
+    }
+}
